e75a4ffb3ac70206fbeaab4eb77059210b0f6372
llomaka <llomaka80@gmail.com>
2021-11-18 18:35:36 +0200

    Виконані завдання 4-6

--- js/task-5.js
+++ js/task-5.js
@@ -1,13 +1,11 @@
-Завдання 5
-Напиши функцію checkForSpam(message), приймаючу 1 параметр message - рядок. Функція перевіряє її на вміст слів spam і sale. Якщо знайшли заборонене слово, то функція повертає true, якщо заборонених слів немає функція повертає false. Слова в рядку можуть бути в довільному регістрі.
+"use strict";
 
 const checkForSpam = function (message) {
-  // твій код
+  if (message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale')) return true;
+  return false;
 };
 
-/*
- * Викличи функції для перевірки працездатності твоєї реалізації.
- */
+
 console.log(checkForSpam('Latest technology news')); // false
 
 console.log(checkForSpam('JavaScript weekly newsletter')); // false
@@ -15,3 +13,5 @@ console.log(checkForSpam('JavaScript weekly newsletter')); // false
 console.log(checkForSpam('Get best sale offers now!')); // true
 
 console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+console.log(checkForSpam('NOT SPAM! INTERNAL HR MESSAGE')); // true
